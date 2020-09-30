b12244c91355d5a3f35fca52f779389905a4e05c
Chris Pressland <mail@cpressland.io>
2020-09-30 16:35:37 +0100

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM binkhq/python:3.8
+
+ADD main.py .
+
+CMD ["python", "main.py"]
+
---
+++ main.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+count = 0
+
+while True:
+    count = count + 1
+    print(count)
+    sleep(1)
+
